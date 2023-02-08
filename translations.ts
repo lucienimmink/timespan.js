b077b100fee4903ab9b876615e34a588b3a83b51
Lucien Immink <lucien@arielext.org>
2023-02-08 11:03:32 +0100

    Add locale support

---
+++ translations.ts
@@ -0,0 +1,30 @@
+export default {
+  en: {
+    day: 'day',
+    days: 'days',
+    hours: 'hr',
+    minutes: 'min',
+    seconds: 'sec',
+  },
+  'en-GB': {
+    day: 'day',
+    days: 'days',
+    hours: 'hr',
+    minutes: 'min',
+    seconds: 'sec',
+  },
+  'nl-NL': {
+    day: 'dag',
+    days: 'dagen',
+    hours: 'uur',
+    minutes: 'min',
+    seconds: 'sec',
+  },
+  nl: {
+    day: 'dag',
+    days: 'dagen',
+    hours: 'uur',
+    minutes: 'min',
+    seconds: 'sec',
+  },
+};
